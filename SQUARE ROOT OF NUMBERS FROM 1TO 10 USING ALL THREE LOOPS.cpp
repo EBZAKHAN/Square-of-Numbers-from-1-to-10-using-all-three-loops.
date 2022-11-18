ef864b0012e1ef53bb04615e53ec0ba8479ee3c5
EBZAKHAN <114294808+EBZAKHAN@users.noreply.github.com>
2022-11-18 22:46:14 +0500

    Add files via upload

---
+++ SQUARE ROOT OF NUMBERS FROM 1TO 10 USING ALL THREE LOOPS.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{{
+	for(int i=1;i<=10;i++)
+	if(i==5)
+	continue;
+	else
+	cout<<"The Square Root Of "<<i<<" is "<<i*i<<endl;
+}
+cout<<"NOW SECOND LOOP TYPE IS"<<endl;
+{
+	int i=1;
+	while(i<=10){
+	
+	i++;
+	if(i==5)
+	continue;
+	else
+	cout<<"The Square Root Of "<<i<<" is "<<i*i<<endl;
+	
+}
+}
+cout<<"NOW THIRD LOOP TYPE IS"<<endl;
+{
+int i=0;
+do
+{
+    i++;
+      if(i==5)
+      continue;
+     cout<<"The Square Root Of "<<i<<" IS"<<i*i<<endl;
+    
+ 
+   }
+    while(i<=9);
+}
+}
+
+
+
+
